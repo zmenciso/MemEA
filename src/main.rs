485eb5f6dc8b83ce4d007b4e3079437ba1e08354
Zephan Enciso <zmenciso@pm.me>
2024-11-22 15:36:15 -0500

    Process multiple config files at once

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use clap::Parser;
 use std::path::PathBuf;
 use std::error::Error;
+use std::collections::HashMap;
+
+const DEFAULT_DB: &str = "./data/db.txt";
 
 mod config;
 mod primitives;
@@ -10,9 +13,9 @@ mod export;
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 pub struct Args {
-    config: PathBuf,
+    config: Vec<PathBuf>,
 
-    #[arg(short, long, default_value = "./data/db.txt")]
+    #[arg(short, long, default_value = DEFAULT_DB)]
     db: PathBuf,
 
     #[arg(short, long)]
@@ -29,19 +32,36 @@ fn main() -> Result<(), Box<dyn Error>> {
     let args = Args::parse();
     let verbose = !args.quiet && !args.area_only;
 
-    export::vprint("Reading configuration...", verbose);
-    let config = config::read(&args.config)?;
+    export::vprint("Reading configuration files...", verbose);
+    let mut config: Vec<config::Config> = Vec::new();
+    for path in args.config {
+        let result = config::read(&path);
+        match result {
+            Ok(t) => config.push(t),
+            Err(e) => {
+                eprintln!("ERROR: Could not parse input {}", e);
+                std::process::exit(5); 
+            }
+        }
+    }
 
     export::vprint("Building database...", verbose);
     let db = primitives::build_db(&args.db)?;
 
     export::vprint("Tabulating solution...", verbose);
-    let report = tabulate::tabulate(config, &db).expect("Could not tabulate solution");
+    let reports: Vec<HashMap<String, f32>> = config.
+        iter().
+        map(|x| tabulate::tabulate(x, &db)).
+        collect();
 
-    match args.area_only {
-        true => println!("{}", export::area(&report)),
-        false => export::export(&args.config, &report, args.export)
-    };
+    assert!(reports.len() == config.len());
+
+    for i in 0..reports.len() {
+        match args.area_only {
+            true => println!("{} {}", config[i].path, export::area(&reports[i])),
+            false => export::export(&config[i].path, &reports[i], &args.export)
+        };
+    }
 
     Ok(())
 }
